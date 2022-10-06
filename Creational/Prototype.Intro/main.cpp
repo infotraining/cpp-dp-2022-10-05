2c8041a46951b396fd86586b4064880eba9f32c3
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-10-06 11:11:53 +0200

    Prototype - CRTP

--- Creational/Prototype.Intro/main.cpp
+++ Creational/Prototype.Intro/main.cpp
@@ -8,10 +8,21 @@ class Engine
 public:
     virtual void start() = 0;
     virtual void stop() = 0;
+    virtual std::unique_ptr<Engine> clone() const = 0;
     virtual ~Engine() = default;
 };
 
-class Diesel : public Engine
+template <typename EngineType, typename EngineBase = Engine>
+class CloneableEngine : public EngineBase
+{
+public:
+    std::unique_ptr<Engine> clone() const override
+    {
+        return std::make_unique<EngineType>(static_cast<const EngineType&>(*this));
+    }
+};
+
+class Diesel : public CloneableEngine<Diesel>
 {
 public:
     virtual void start() override
@@ -25,7 +36,7 @@ public:
     }
 };
 
-class TDI : public Diesel
+class TDI : public CloneableEngine<TDI, Diesel>
 {
 public:
     virtual void start() override
@@ -39,7 +50,7 @@ public:
     }
 };
 
-class Hybrid : public Engine
+class Hybrid : public CloneableEngine<Hybrid>
 {
 public:
     virtual void start() override
@@ -63,6 +74,10 @@ public:
     {
     }
 
+    Car(const Car& other) : engine_{other.engine_->clone()}
+    {
+    }
+
     void drive(int km)
     {
         engine_->start();
@@ -73,8 +88,11 @@ public:
 
 int main()
 {
-    Car c1{std::make_unique<Hybrid>()};
+    Car c1{std::make_unique<TDI>()};
     c1.drive(100);
 
     std::cout << "\n";
+
+    Car c2 = c1;
+    c2.drive(200);
 }
